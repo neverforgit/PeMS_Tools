448d70c4ded36c0584e72f999666bebde6e4fe67
twowheelmotion <andrew.campbell@berkeley.edu>
2015-06-04 17:49:35 -0700

    added scrapy
    Former-commit-id: 6b290f1ec1947a855754a90efce5afe9e37bf8fa

---
+++ PeMS_spyder/PeMS_spyder/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for PeMS_spyder project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'PeMS_spyder'
+
+SPIDER_MODULES = ['PeMS_spyder.spiders']
+NEWSPIDER_MODULE = 'PeMS_spyder.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'PeMS_spyder (+http://www.yourdomain.com)'
